7b27e2dd4162f542886f9c93ad2bbe9bda60733a
FunnySkillz <sebastijan.bogdan@outlook.com>
2025-10-21 00:24:35 +0200

    add uow

--- FileService.Persistance/UnitOfWork.cs
+++ FileService.Persistance/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FileService.Core.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace FileService.Persistance
 {
-    internal class UnitOfWork
+    public sealed class UnitOfWork : IUnitOfWork
     {
+        private readonly AppDbContext _db;
+        public IStoredFileRepository StoredFiles { get; }
+
+        public UnitOfWork(AppDbContext db, IStoredFileRepository storedFiles)
+        {
+            _db = db;
+            StoredFiles = storedFiles;
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken ct = default) => _db.SaveChangesAsync(ct);
+
+        public ValueTask DisposeAsync() => _db.DisposeAsync();
     }
 }
